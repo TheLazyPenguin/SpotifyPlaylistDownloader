6d4e4473c0883c96548da66c36f427b2addc113a
TheLazyPenguin <tigerclaw49@gmail.com>
2018-08-21 23:23:03 +0200

    Urgent Update pd
    Temporarily deleted client_id,client_secret and username

--- SpotifyPlaylistDownloader.py
+++ SpotifyPlaylistDownloader.py
@@ -17,10 +17,10 @@ def editPlaylist(results):
     file.write(jsonarray)
     file.close
 Playlist_ID= input("What is ID? ")
-username="lazywolf49@outlook.com"
-token = util.prompt_for_user_token(username=username,scope="playlist-read-collaborative",client_id="eaa2b856a56a495699ac28193ea694fa",client_secret="447d8103980f40e4a0d21497b6d2bd70",redirect_uri="https://youtube.com")
+userna
+token = util.prompt_for_user_token(username=username,scope="playlist-read-collaborative",client_id="",client_secret=,redirect_uri="https://youtube.c
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.user_playlist_tracks(playlist_id=Playlist_ID,limit=100,offset=0,user=username)
+    results = sp.user_playlist_tracks(playlist_id=Playlist_ID,limit=10offset=0,user=username)
     editPlaylist(results)
 
