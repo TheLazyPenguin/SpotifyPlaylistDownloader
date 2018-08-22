a00c455a78d85048c8367eed8106fac682a96ce4
lazyw_000 <tigerclaw49@gmail.com>
2018-08-22 09:36:55 +0200

    Fixed username,client_id and client_secret and redirect_uri from showing in the code. User needs to input it themselves.

--- SpotifyPlaylistDownloader.py
+++ SpotifyPlaylistDownloader.py
@@ -2,6 +2,7 @@ import spotipy
 import sys
 from spotipy import util
 import json
+import config as cg
 def editPlaylist(results):
     for i in range(46):
         del results['items'][i]['added_at']
@@ -17,10 +18,9 @@ def editPlaylist(results):
     file.write(jsonarray)
     file.close
 Playlist_ID= input("What is ID? ")
-username="lazywolf49@outlook.com"
-token = util.prompt_for_user_token(username=username,scope="playlist-read-collaborative",client_id="eaa2b856a56a495699ac28193ea694fa",client_secret="447d8103980f40e4a0d21497b6d2bd70",redirect_uri="https://youtube.com")
+token = util.prompt_for_user_token(username=cg.username,scope="playlist-read-collaborative",client_id=cg.client_id,client_secret=cg.client_secret,redirect_uri=cg.redirect_uri)
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.user_playlist_tracks(playlist_id=Playlist_ID,limit=100,offset=0,user=username)
+    results = sp.user_playlist_tracks(playlist_id=Playlist_ID,limit=100,offset=0,user=cg.username)
     editPlaylist(results)
 
---
+++ config.py
@@ -0,0 +1,5 @@
+"Enter necessary credentials"
+username=""
+client_id=""
+client_secret=""
+redirect_uri=""
